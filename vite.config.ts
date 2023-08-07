5c3b929d4e7f4ec5a1beb37aca3857c5a5bca9b5
Andi <andisurdu23@gmail.com>
2023-08-07 16:24:16 +0300

    Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
     // Required for Github-Pages:
-    base: "React-Notes",
+    base: "/React-Notes",
     plugins: [react()],
     css: {
         devSourcemap: true,
